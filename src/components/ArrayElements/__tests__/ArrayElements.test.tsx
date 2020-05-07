423b01fb9ed30c8b0af81e3c33e6e3f194bc297a
ElvisHernandez <ElvisHernandezTheone@gmail.com>
2020-05-07 18:18:42 -0400

    added test to make sure makeArray button produces a new array

--- src/components/ArrayElements/__tests__/ArrayElements.test.tsx
+++ src/components/ArrayElements/__tests__/ArrayElements.test.tsx
@@ -1,12 +1,21 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import ArrayElements from "../ArrayElements";
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 
 jest.mock("../ArrayElements.module.css", () => ({
   bars: "bars",
   content: "content",
 }));
+
+function arraysAreEqual(array1: number[], array2: number[]): boolean {
+  if (array1.length !== array2.length) return false;
+
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) return false;
+  }
+  return true;
+}
 
 describe("Test ArrayElement component", () => {
   test("Should render without crashing", () => {
@@ -32,5 +41,27 @@ describe("Test ArrayElement component", () => {
     expect(components).toHaveLength(100);
   });
 
-  test("");
+  test("Should produce ArrayElement components of different heights when Make Array is clicked", () => {
+    const { getAllByTestId, getByText } = render(<ArrayElements />);
+    const components = getAllByTestId("array-element");
+
+    const defaultHeights: number[] = [];
+    components.forEach(({ style: { height } }) => {
+      const elementHeight = +height.slice(0, -2);
+      defaultHeights.push(elementHeight);
+    });
+
+    const button = getByText("Make Array");
+    fireEvent.click(button);
+    const newComponents = getAllByTestId("array-element");
+    const newHeights: number[] = [];
+
+    newComponents.forEach(({ style: { height } }) => {
+      const elementHeight = +height.slice(0, -2);
+      newHeights.push(elementHeight);
+    });
+
+    const areEqual = arraysAreEqual(defaultHeights, newHeights);
+    expect(areEqual).toEqual(false);
+  });
 });
